44211e612d824a2413352e3882bdce587d42d208
ahtn <jem@seethis.link>
2018-01-28 19:58:04 +1000

    fix flash size flag not set correctly

--- avr-xmega.mk
+++ avr-xmega.mk
@@ -38,7 +38,7 @@ else
   $(error No part matches MCU='$(MCU)')
 endif
 
-CDEFS += -DMCU_FLASH_SIZE=128
+CDEFS += -DMCU_FLASH_SIZE=$(MCU_FLASH_SIZE)
 CDEFS += -DMCU_STRING=\"$(MCU_STRING)\"
 
 ifndef AVRDUDE_DEVICE_FUSES
