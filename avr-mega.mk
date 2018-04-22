4709f63f7283fb7240204db44968b0f59e21b138
ahtn <jem@seethis.link>
2018-04-22 20:26:55 +1000

    add lock bit to avr-mega

--- avr-mega.mk
+++ avr-mega.mk
@@ -73,9 +73,13 @@ else ifeq ($(BOOTSZ_TYPE), 1)
   endif
 endif
 
-# ifndef AVRDUDE_DEVICE_FUSES
-AVRDUDE_DEVICE_FUSES=\
-    -U lfuse:w:"0x$(LFUSE)":m \
-    -U hfuse:w:"0x$(HFUSE)":m \
-    -U efuse:w:"0x$(EFUSE)":m \
-# endif
+ifndef AVRDUDE_DEVICE_FUSES
+    AVRDUDE_DEVICE_FUSES=\
+        -U lfuse:w:"0x$(LFUSE)":m \
+        -U hfuse:w:"0x$(HFUSE)":m \
+        -U efuse:w:"0x$(EFUSE)":m
+endif
+
+ifndef AVRDUDE_DEVICE_LOCK
+    AVRDUDE_DEVICE_LOCK=-U lock:w:"0x$(LOCKBITS)":m
+endif
